a06a0d96063a16175acb779e9e890a78601c1058
hhduisldv <zhouyichi1028@icloud.com>
2023-02-18 21:03:29 +0800

    Initial commit

---
+++ src/com/common/User.java
@@ -0,0 +1,37 @@
+package com.common;
+
+
+public class User implements java.io.Serializable {
+
+    private String uid;
+    private String pwd;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public User(String uid, String pwd) {
+        this.uid = uid;
+        this.pwd = pwd;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid='" + uid + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+}
